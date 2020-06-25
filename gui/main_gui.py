7b17342d60be3f02ad8cc2a54e99d303c650d0d3
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-06-25 10:27:03 -0300

    Valor inicial de sliders por comando

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -30,30 +30,91 @@ from components.MainWindow import MainWindow
 from components.Sliders import Sliders
 from components.InfoLabels import InfoLabels
 from components.Plot import Plot
+from datetime import datetime
 import logging
 import re
+
+def get_params(argv):
+    """Method that translates the expected command line parameters to a dictionary
+
+    :param argv: The command line parameters
+    :type argv: list
+    :return: A dictionary containing the command line parameters that were passed
+    :rtype: dictionary or None if there were not parameters found
+    """
+    argv_string = ' '.join(argv)    #   Get as one string to match pattern
+    #   Get logging arguments and other through command line arguments as dictionary.
+    #   The '(?P<>)' part is for named groups, see: https://docs.python.org/3/howto/regex.html
+    log_pattern = r"--log=(?P<log_level>DEBUG\b|INFO\b|WARNING\b|ERROR\b|CRITICAL\b)"
+    kp_pattern = r"--kp=(?P<kp>[0-9]+.?[0-9]*)"
+    ki_pattern = r"--ki=(?P<ki>[0-9]+.?[0-9]*)"
+    kd_pattern = r"--kd=(?P<kd>[0-9]+.?[0-9]*)"
+    speed_pattern = r"--speed=(?P<speed>[0-9]+)" #   Only integers
+
+    params_dict = None
+    for pattern, name in ([log_pattern, "log_level"],   #   Carefull! If names change, patterns will have to change
+                    [kp_pattern, "kp"],
+                    [ki_pattern, "ki"],
+                    [kd_pattern, "kd"],
+                    [speed_pattern, "speed"]):
+        match = re.search(pattern, argv_string)
+        if match:
+            if params_dict is None:
+                params_dict = {}
+                params_dict[name] = match.groupdict()[name]
+            else:
+                params_dict[name] = match.groupdict()[name]
+            
+    return params_dict
 
 def main(argv):
     """Function that creates the interface in charge of managing the engine that is connected
     through a socket at localhost:8080.
 
-    :param argv: The command line arguments to be processes. It expects nothing or --log=LOG_LEVEL
-    where LOG_LEVEL can be DEBUG, INFO, WARNING, ERROR or CRITICAL, upper or lower case. If not
-    passed, the default value is WARNING
+    :param argv: The command line arguments to be processes. It expects:
+        --log=LOG_LEVEL
+            where LOG_LEVEL can be DEBUG, INFO, WARNING, ERROR or CRITICAL, upper or lower case. If not
+            passed, the default value is WARNING.
+        --kp=value
+        --ki=value
+        --kd=value
+        --speed=value
+            If kp, ki, kd, and/or speed are not passed, their default value is 0
     :type argv: str
     """
-    #   Get logging arguments through command line arguments as dictionary.
-    #   The '(?P<>)' part is for named groups, see: https://docs.python.org/3/howto/regex.html
-    log_pattern = r"--log=(?P<log_level>DEBUG\b|INFO\b|WARNING\b|ERROR\b|CRITICAL\b)"
-    regex = re.compile(log_pattern, flags=re.I)
-    argv_string = ' '.join(argv)    #   Get as one string to match pattern
-    result = regex.search(argv_string)
-    if (result == None):
+    params_dict = get_params(argv)
+    print(params_dict)
+    #   Set defaults or passed values
+    if (params_dict == None):
         log_level = "WARNING"
+        kp_init = 0.0
+        ki_init = 0.0
+        kd_init = 0.0
+        speed_init = 0
     else:
-        log_level = result.groupdict()['log_level'].upper()
+        if 'log_level' in params_dict:
+            log_level = params_dict['log_level'].upper()
+        else:
+            log_level = "WARNING"
+        if 'kp' in params_dict:
+            kp_init = float(params_dict['kp'])
+        else:
+            kp_init = 0.0
+        if 'ki' in params_dict:
+            ki_init = float(params_dict['ki'])
+        else:
+            ki_init = 0.0
+        if 'kd' in params_dict:
+            kd_init = float(params_dict['kd'])
+        else:
+            kd_init = 0.0
+        if 'speed' in params_dict:
+            speed_init = int(params_dict['speed'])
+        else:
+            speed_init = 0
     #   Set logging config
     logging.basicConfig(filename="logs" ,level=log_level)
+    logging.info("Starting program at time: {}".format(datetime.now()))
     #   Create socket client and inject to connection
     client = Client('localhost', 8080)
     connection = Connection(client)
@@ -63,6 +124,11 @@ def main(argv):
     sliders.set_observer(connection.watch_slider, 'i')
     sliders.set_observer(connection.watch_slider, 'd')
     sliders.set_observer(connection.watch_slider, 'v')
+    #   Set the sliders to the initial values
+    sliders.scales['p'].set_value(kp_init)
+    sliders.scales['i'].set_value(ki_init)
+    sliders.scales['d'].set_value(kd_init)
+    sliders.scales['v'].set_value(speed_init)
     #   Create and set speed observer
     speed_observer = SpeedObserver()
     sliders.set_observer(speed_observer.watch_slider, 'v')
